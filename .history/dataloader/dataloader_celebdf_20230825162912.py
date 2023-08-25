81966b4411822f5d330276929dd86fc0cd6f1082
Zhougz <2375950647@qq.com>
2023-08-25 16:37:45 +0800

    make ffpp, celebdf runnable

---
+++ .history/dataloader/dataloader_celebdf_20230825162912.py
@@ -0,0 +1,170 @@
+import os
+import cv2
+import glob
+import numpy as np
+import pandas as pd
+from PIL import Image
+import torch
+import torch.utils.data as data
+import sys
+sys.path.append("./")
+from utils.config import *
+
+
+class CeleDF(data.Dataset):
+    """DFDC Detection Dataset Object
+    input is image, target is annotation
+    Arguments:
+        root (string): filepath to VOCdevkit folder.
+        train (bool): imageset to use (eg. 'train', test')
+        transform (callable, optional): transformation to perform on the
+            input image
+        target_transform (callable, optional): transformation to perform on the
+            target `annotation`
+            (eg: take in caption string, return tensor of word indices)
+        dataset_name (string, optional): which dataset to load
+            (default: 'DFDC')
+    """
+
+    def __init__(self, train=True, frame_nums=5,
+                 transform=None, target_transform=None,data_root = '/media/sdc/datasets/celedf/',Deepfakes_path ='/Celeb-synthesis-mtcnn/*',
+        original_path = '/Celeb-real-mtcnn/*',
+        youtube_path ='/YouTube-real-mtcnn/*',
+        split_path = '/List_of_testing_videos.txt', dataset_name='CELEDF'):
+        self.train = train
+        self.frame_nums = frame_nums
+        self.transform = transform
+        self.target_transform = target_transform
+        self.type = dataset_name
+        self.data_root = data_root
+        self.split_path = split_path
+        self.Deepfakes_path = Deepfakes_path
+        self.original_path = original_path
+        self.youtube_path = youtube_path
+        self.datas = self.init()
+       
+
+    def __getitem__(self, index):
+        img_path, target, folder = self.datas[index]
+
+
+        img = Image.open(img_path)
+
+        if self.transform is not None:
+            img = self.transform(img)
+
+        if self.target_transform is not None:
+            target = self.target_transform(target)
+
+        return img, target,folder,img_path
+
+    def __len__(self):
+        return len(self.datas)
+
+    def init(self):
+        datas = []
+        path_list = []
+
+        # Deepfakes_path = f'{self.data_root}/Celeb-synthesis-mtcnn/*'
+        # original_path = f'{self.data_root}/Celeb-real-mtcnn/*'
+        # youtube_path = f'{self.data_root}/YouTube-real-mtcnn/*'
+        # split_path = f'{self.data_root}/List_of_testing_videos.txt'
+        data_root = self.data_root
+        with open(self.split_path,'r') as f:
+            self.raw_list = f.read().splitlines()
+        test_list = [x.split(" ")[1] for x in self.raw_list]
+        test_list = [data_root+'/'+x.split("/")[1][:-4] for x in test_list]
+
+        # path_list.append(self.Deepfakes_path)
+        # path_list.append(self.original_path)
+        # path_list.append(self.youtube_path)
+        self.fake_num = 0
+        self.real_num = 0
+        if self.train == True:
+            
+            for path in test_list:
+                # folder_paths_all = glob.glob(path)
+                # folder_paths = np.setdiff1d(folder_paths_all,test_list)
+                label_str = path.split('/')[5]
+                
+                label = 1 if len(label_str) >13 else 0
+                # for folder in folder_paths:
+                if label == 1:
+                    self.fake_num = self.fake_num+1
+                else:
+                    self.real_num = self.real_num+1
+                face_paths = glob.glob(os.path.join(path, '*.png'))
+
+                if len(face_paths) < 5:
+                    continue
+                if len(face_paths) > self.frame_nums:
+                    face_paths = np.array(sorted(face_paths, key=lambda x: int(x.split('/')[-1].split('.')[0][-4:])))
+                    ind = np.linspace(0, len(face_paths) - 1, self.frame_nums, endpoint=True, dtype=np.int)
+                    face_paths = face_paths[ind]
+
+                datas.extend([[face_path, label,path] for face_path in face_paths])
+        else:
+            folder_paths = test_list
+            
+            for folder in folder_paths:
+                label_str = folder.split('/')[5]
+                label = 1 if label_str == 'Celeb-synthesis-mtcnn' else 0
+                
+                if label == 1:
+                        self.fake_num = self.fake_num+1
+                else:
+                        self.real_num = self.real_num+1
+                face_paths = glob.glob(os.path.join(folder, '*.png'))
+
+                if len(face_paths) < 5:
+                    continue
+                if len(face_paths) > self.frame_nums:
+                    face_paths = np.array(sorted(face_paths, key=lambda x: int(x.split('/')[-1].split('.')[0][-4:])))
+                    ind = np.linspace(0, len(face_paths) - 1, self.frame_nums, endpoint=True, dtype=np.int)
+                    face_paths = face_paths[ind]
+
+                datas.extend([[face_path, label,folder] for face_path in face_paths])
+            
+        return datas
+
+
+# def load_FaceForens(train_size=128, test_size=1, transform=None):
+#     if transform == None:
+#         from transforms import ResNet_default_data_transforms
+#         transform = ResNet_default_data_transforms
+
+#     trans = transform['train']
+#     train_dataset = DFDCDetection(train=True, frame_nums=5, transform=trans)
+#     train_dataloader = data.DataLoader(train_dataset, batch_size=train_size, shuffle=True, num_workers=4)
+
+#     trans = transform['test']
+#     test_dataset = DFDCDetection(train=False, frame_nums=12, transform=trans)
+#     test_dataloader = data.DataLoader(test_dataset, batch_size=test_size, shuffle=True, num_workers=4)
+
+#     return train_dataloader, test_dataloader
+
+
+if __name__ == '__main__':
+    from transform import get_transform
+
+    trans = get_transform(input_size)['train']
+    train_dataset = CeleDF(train=True, frame_nums=3, transform=trans, data_root=celebdf_path, split_path = split_path )
+    print(f"train dataset real :{train_dataset.real_num},fake :{train_dataset.fake_num}")
+    train_dataloader = data.DataLoader(train_dataset, batch_size=64, shuffle=True, num_workers=4)
+    #pdb.set_trace()
+    print(len(train_dataset))
+    for i, datas in enumerate(train_dataloader):
+        print(i, datas[0].shape, datas[1].shape)
+        break
+
+    trans = get_transform(input_size)['test']
+    test_dataset = CeleDF(train=False, frame_nums=3, transform=trans)
+    print(f"test dataset real :{test_dataset.real_num},fake :{test_dataset.fake_num}")
+
+    test_dataloader = data.DataLoader(test_dataset, batch_size=64, shuffle=True, num_workers=4)
+    print(len(train_dataset))
+    print(len(test_dataset))
+    for i, datas in enumerate(test_dataloader):
+        print(i, datas[0].shape, datas[1].shape)
+        print(datas[1])
+        break
---
+++ .history/utils/config_20230825144530.py
@@ -0,0 +1,48 @@
+ffpp_original_path = "/data1/chenpeng/data/FaceForensics++/original_sequences/youtube"
+ffpp_fake_path = "/data1/chenpeng/data/FaceForensics++/manipulated_sequences"
+celebdf_path = "/data5/tjh/standard_crop/Celeb-DFv2"
+dfdc_path = "/data5/tjh/standard_crop/select_dfdc_test/videos"
+#celeb-df
+Deepfakes_path = f'{self.data_root}/Celeb-synthesis-mtcnn/*'
+original_path = f'{self.data_root}/Celeb-real-mtcnn/*'
+youtube_path = f'{celebdf_path}/YouTube-real-mtcnn/*'
+split_path = f'{self.celebdf_path}/test.txt'
+DETERMINSTIC = True
+RNG_SEED = 20
+batch_size = 128
+frame_nums = 10
+
+compress = 'c23'    #choose c23 or c40
+
+input_size = 224
+#TODO: mtcnn is used for?
+detect_name = "mtcnn"
+model_name = 'efficientnet-b4'
+type_list = ['Deepfakes','Face2Face','FaceSwap','NeuralTextures','all']
+type_list_short = ['df','f2f','fs','nt','all']
+real_weight = 1
+
+
+model_path = None
+fnet_path = None
+
+beta1 = 0.9
+beta2 = 0.999
+learning_rate = 0.001
+metalr = 0.001
+plr = 0.001
+epochs = 200
+step_size = 5
+gamma = 0.1
+weight_bce = 0.05
+weight_ct = 1
+# Misc
+print_interval = 100
+save_interval = 5
+test_interval = 1
+finetune_interval = 2
+parallel = True
+lamda = 0.01
+alpha = 1
+des = "train"
+save_dir = f"result/output_{compress}_GCD_ablation/{compress}_{model_name}_{input_size}_{lamda}_{alpha}_{des}"
