325ff09c11eefb964dc4c37f5fecfbc90d889104
Zhougz <2375950647@qq.com>
2023-08-27 21:37:22 +0800

    FIX LOAD PRETRAIN BUG

--- train.py
+++ train.py
@@ -29,11 +29,6 @@ from utils.roc import cal_metric
 from utils import *
 import time
 os.environ['CUDA_VISIBLE_DEVICES']='0,1'#important
-from torch.nn.parallel import DistributedDataParallel as DDP
-import apex 
-# from apex.optimizers import FP16_Optimizer
-# from apex.optimizers import FusedAdam
-# from transformers import get_linear_schedule_wit_warmup
 
 def worker_init_fn(x):
     seed = RNG_SEED
@@ -54,19 +49,17 @@ def model_forward(image, model, post_function=nn.Sigmoid(),feat = False):
     """
     # start_time = time.time()
     # Model prediction
-    # if fp16:
-    #     image = image.half()
-    with torch.cuda.amp.autocast():
-        output,feature = model(image)
+    output,feature = model(image)
     #TODO: RI 
-        output = post_function(output)
-        output = output.squeeze()
-        prediction = (output >= 0.5).float()
+    output = post_function(output)
+    output = output.squeeze()
+    
+    prediction = (output >= 0.5).float()
     # print(f"model_forward took {time.time() - start_time} seconds")
     if feat==False:
         return prediction, output
     else:
-        return prediction, output, feature
+        return prediction, output,feature
 
 
 def train(model,optimizer,fnet,optimizer_fnet,train_dataloader,meta_dataloader,criterion_oc,epoch,epoch_size,device):
@@ -85,7 +78,6 @@ def train(model,optimizer,fnet,optimizer_fnet,train_dataloader,meta_dataloader,c
     meta_lr = metalr * ((gamma ** int(epoch >= step_size)) * (gamma ** int(epoch >= step_size*2)) * (gamma ** int(epoch >= step_size*3)) * (gamma ** int(epoch >= step_size*4)))         
     
     print(f"meta lr = {meta_lr}")  
-    scaler = torch.cuda.amp.GradScaler()
 
     for i, datas in enumerate(train_dataloader):
         
@@ -99,30 +91,28 @@ def train(model,optimizer,fnet,optimizer_fnet,train_dataloader,meta_dataloader,c
             meta_model.copyModel(model.module)
         else:
             meta_model.copyModel(model)
-        
-     
         prediction, output,feature = model_forward(images,model,feat = True)
-        targets = targets.to(output, non_blocking=True)
         compact_loss = criterion_oc(output,targets)
+
         cost = criterion(output, targets)
         cost_v = torch.reshape(cost, (len(cost), 1))
-        with torch.cuda.amp.autocast():
-            f_lambda = fnet(feature)
 
-            f_lambda_norm = nn.Sigmoid()(f_lambda)
+        f_lambda = fnet(feature)
 
-            l_f_model = torch.sum(cost_v)/len(cost_v) + lamda * compact_loss
+        f_lambda_norm = nn.Sigmoid()(f_lambda)
 
-        model.zero_grad()
+        l_f_model = torch.sum(cost_v)/len(cost_v) + lamda * compact_loss
+
         
+        model.zero_grad()
         if parallel:
             grads = torch.autograd.grad(l_f_model, (model.module.params()),create_graph=True)
         else:
             grads = torch.autograd.grad(l_f_model, (model.params()),create_graph=True)
-        
         #Virtual update model
         meta_model.update_params(lr_inner=meta_lr, source_params=grads,solver = 'adam')
         
+        del grads
         if meta_dataloader!=None:
             try:
                 meta_datas = next(meta_dataloader_iter)
@@ -146,31 +136,20 @@ def train(model,optimizer,fnet,optimizer_fnet,train_dataloader,meta_dataloader,c
             meta_targets = opposite_targets
         
         prediction_meta, output_mata = model_forward(meta_images,meta_model)
-        
         l_g_meta = criterion_norm(output_mata, meta_targets)
-        with torch.cuda.amp.autocast():
-            with torch.no_grad():
-                w_new = fnet(feature)
-                w_new_norm = nn.Sigmoid()(w_new)
+        with torch.no_grad():
+            w_new = fnet(feature)
+            w_new_norm = nn.Sigmoid()(w_new)
 
         loss = torch.sum(cost_v * (w_new_norm+1))/len(cost_v)
 
         loss_add = loss + alpha*l_g_meta + lamda*compact_loss
-        
         start_time = time.time()
-        
         optimizer.zero_grad()
         optimizer_fnet.zero_grad()
-        
-        scaler.scale(loss_add).backward(retain_graph=True)
-        # scaler.scale(loss_add)
-        
-        scaler.step(optimizer)
-        optimizer_fnet.step()
-
-        # Updates the scale for next iteration
-        scaler.update()
-          
+        loss_add.backward()
+        optimizer.step()
+        optimizer_fnet.step()     
         print(f"backward steps took {time.time()-start_time} seconds") #backward steps took 8.353310585021973 seconds
         acc = (prediction==targets).float().mean()
         meta_acc = (prediction_meta==meta_targets).float().mean()
@@ -245,10 +224,7 @@ def updatebest(metrics,best_acc,best_loss,name,model,pnet):
     return best_acc,best_loss
 
 
-def main():#param = rank,world_size
-    # print(f"Running basic DDP example on rank {rank}.")
-    # setup(rank, world_size)
-    
+def main():
     #TODO: save yaml config
     # save_dir = save_dir + '/'+ str(time.time())
     if os.path.exists(save_dir):
@@ -262,13 +238,9 @@ def main():#param = rank,world_size
 
     fnet = FNet(model.num_ftrs).to(device)
 
-
-    if model_path is not None:    
-        state_dict = torch.load(model_path)
-        model.load_state_dict(state_dict)
-        print(f'resume model from {model_path}')
-    else:
-        print('No model found, initializing random model.')
+    # save_checkpoint(model.state_dict(), fpath=f'{save_dir}/{model_name}_lastepoch.pth')
+    # save_checkpoint(fnet.state_dict(), fpath=f'{save_dir}/{model_name}_pnet_lastepoch.pth')
+    
     if fnet_path is not None:    
         state_dict = torch.load(fnet_path)
         fnet.load_state_dict(state_dict)
@@ -276,51 +248,24 @@ def main():#param = rank,world_size
     else:
         print('No fnet_model found, initializing random model.')
 
-    
-    # if fp16:
-    #     model.half()
     model = model.to(device)
-    
     if parallel:
         model = nn.DataParallel(model)
-    """
-    choose optimizer depending on precision type
-    """
-    # param_optimizer = list(model.named_parameters())
-    # fnet_param_optimizer =  list(fnet.named_parameters())
-    # no_decay = ['bias', 'LayerNorm.bias', 'LayerNorm.weight']
-    # optimizer_grouped_parameters = [
-    #     {'params': [p for n, p in param_optimizer if not any(nd in n for nd in no_decay)], 'weight_decay': 0.01},
-    #     {'params': [p for n, p in param_optimizer if any(nd in n for nd in no_decay)], 'weight_decay': 0.0}
-    #     ]
-    # fnet_optimizer_grouped_parameters = [
-    #     {'params': [p for n, p in fnet_param_optimizer if not any(nd in n for nd in no_decay)], 'weight_decay': 0.01},
-    #     {'params': [p for n, p in fnet_param_optimizer if any(nd in n for nd in no_decay)], 'weight_decay': 0.0}
-    #     ]
-    # if fp16:
-        
-    #     optimizer = FusedAdam(optimizer_grouped_parameters,
-    #                             lr=learning_rate,
-    #                             bias_correction=False,
-    #                             max_grad_norm=1.0,
-    #                             betas=(beta1,beta2))
-    #     optimizer_fnet = FusedAdam(fnet_optimizer_grouped_parameters, lr=plr, bias_correction=False,
-    #                             max_grad_norm=1.0,betas=(beta1,beta2))
-    #     if loss_scale == 0:
-    #         optimizer = FP16_Optimizer(optimizer, dynamic_loss_scale=True)
-    #         optimizer = FP16_Optimizer(optimizer_fnet, dynamic_loss_scale=True)
-    #     else:
-    #         optimizer = FP16_Optimizer(optimizer, static_loss_scale=loss_scale)
-    #         optimizer = FP16_Optimizer(optimizer_fnet, static_loss_scale=loss_scale)
-
-    # else:
+    
+    ##load nn.parallel model
+    if model_path is not None:    
+        state_dict = torch.load(model_path)
+        model.load_state_dict(state_dict)
+        print(f'resume model from {model_path}')
+    else:
+        print('No model found, initializing random model.')
+    
     if parallel:
         optimizer = torch.optim.Adam(model.module.params(), lr=learning_rate, betas=(beta1,beta2))
     else:
         optimizer = torch.optim.Adam(model.params(), lr=learning_rate, betas=(beta1,beta2))
 
     optimizer_fnet = torch.optim.Adam(fnet.params(), lr=plr, betas=(beta1,beta2))
-
 
     scheduler = lr_scheduler.StepLR(optimizer, step_size=step_size, gamma=gamma)
     scheduler_fnet = lr_scheduler.StepLR(optimizer_fnet, step_size=step_size, gamma=gamma)
@@ -358,7 +303,7 @@ def main():#param = rank,world_size
 
 
     model.train()
-    
+   
     best_acc = 0.
     best_loss = 1000.
     domain_best_acc = 0.
@@ -384,9 +329,7 @@ def main():#param = rank,world_size
         epoch_size = len(train_dataset) //batch_size
         print(f"train dataset is:{copydatalist[0].type},{copydatalist[1].type},meta dataset is:{meta_dataset.type}")
         train_dataloader = data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=8,worker_init_fn=worker_init_fn)
-
-        # warmup_linear = get_linear_schedule_with_warmup(optimizer, num_warmup_steps = warm_up_ratio * epoch_size, num_training_steps = epoch_size)
-        # fnet_warmup_linear = get_linear_schedule_with_warmup(optimizer_fnet, num_warmup_steps = warm_up_ratio * epoch_size, num_training_steps = epoch_size) 
+    
         train(model,optimizer,fnet,optimizer_fnet,train_dataloader,None,criterion_oc,epoch,epoch_size,device)
         #train2(model,optimizer,train_dataloader,criterion,epoch,epoch_size,device,meta_dataloader=None)
 
@@ -435,16 +378,6 @@ def main():#param = rank,world_size
             save_checkpoint(fnet.state_dict(), fpath=f'{save_dir}/{model_name}_pnet_lastepoch.pth')
             
     print(f'save dir :{save_dir} done!!!')
-    
-def setup(rank, world_size):
-    os.environ['MASTER_ADDR'] = 'localhost'
-    os.environ['MASTER_PORT'] = master_port
-
-    # initialize the process group
-    dist.init_process_group("gloo", rank=rank, world_size=world_size)
-
-def cleanup():
-    dist.destroy_process_group()
 
 if __name__ == '__main__':
     if DETERMINSTIC:
@@ -457,7 +390,3 @@ if __name__ == '__main__':
         torch.backends.cudnn.deterministic = True         
         torch.backends.cudnn.enabled = False   
     main()
-    # mp.spawn(main,
-    #          args=(world_size,),
-    #          nprocs=world_size,
-    #          join=True)
